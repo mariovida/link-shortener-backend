75ba565e8c803e6876c62883c897cfcedf7103bb
Mario <mariovidovic97@gmail.com>
2025-08-12 19:10:03 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { nanoid } from "nanoid";
+import { pool } from "./db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Create short URL
+app.post("/api/shorten", async (req, res) => {
+  const { url, customSlug } = req.body;
+  if (!url) return res.status(400).json({ error: "URL is required" });
+
+  let slug = customSlug?.trim() || nanoid(6).toLowerCase();
+
+  try {
+    const [rows] = await pool.query("SELECT slug FROM links WHERE slug = ?", [
+      slug,
+    ]);
+    if (rows.length > 0)
+      return res.status(400).json({ error: "Slug already in use." });
+
+    await pool.query(
+      "INSERT INTO links (slug, url, clicks, created_at) VALUES (?, ?, 0, NOW())",
+      [slug, url]
+    );
+
+    res.json({ shortUrl: `http://localhost:${PORT}/${slug}` });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Redirect route
+app.get("/:slug", async (req, res) => {
+  const { slug } = req.params;
+  try {
+    const [rows] = await pool.query(
+      "SELECT url, clicks FROM links WHERE slug = ?",
+      [slug]
+    );
+    if (rows.length === 0) return res.status(404).send("Not found");
+
+    const url = rows[0].url;
+    const clicks = rows[0].clicks;
+
+    pool.query("UPDATE links SET clicks = ? WHERE slug = ?", [
+      clicks + 1,
+      slug,
+    ]);
+
+    res.redirect(url);
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
